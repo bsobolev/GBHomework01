3436a4a7ec0dfcc388b1f6bfc2c683621b584302
Rafinad <rafinad@outlook.com>
2021-07-03 15:31:14 +0300

    hw1,2

--- GBLesson02/2l_ФИ.playground/Contents.swift
+++ GBLesson02/2l_ФИ.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground!"
+// Задание 1
+
+let chislo = 12
+
+func chetNeChet() {
+    let ostatok = chislo % 2
+    ostatok == 1 ? print("Число \(chislo) является нечетным") : print("Число \(chislo) является четным")
+    
+}
+chetNeChet()
+
+// Задание 2
+
+func bezOstatkaNaTri() {
+    let ostatok = chislo % 3
+    ostatok == 0 ? print("Число \(chislo) делится на 3 без остатка") : print("Число \(chislo) не делится на 3 без остатка")
+    
+}
+
+
+bezOstatkaNaTri()
